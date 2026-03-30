a4cccc3b8f7bea7b20881cd6440c2aa40eb47891
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-03-30 02:32:40 -0400

    implementacion de los metodos para la logica de negocio relacionada al CRUD de Producto

--- backend/src/Modules/Inventory/Inventory.Application/Services/ProductService.cs
+++ backend/src/Modules/Inventory/Inventory.Application/Services/ProductService.cs
@@ -34,8 +34,87 @@ public class ProductService : IProductService
             p.MinStockAlert,
             p.IsActive,
             p.IsSoldOut,
-            p.Category?.Name ?? "Sin Categoría", 
+            p.CategoryId,
+            p.Category?.Name ?? "Sin Categoría",
+            p.UnitId,
             p.Unit?.Name ?? "Sin Unidad"
             )).ToList();
+    }
+
+    public async Task<ProductLookUpDto> CreateProductAsync(ProductCreateDto dto)
+    {
+        if (string.IsNullOrWhiteSpace(dto.Sku)) throw new ArgumentException("El codigo sku es obligatorio");
+        if(string.IsNullOrWhiteSpace(dto.Name)) throw new ArgumentException("El nombre es obligatorio");
+        
+        bool skuExists = await _repository.ExistsBySkuAsync(dto.CompanyId, dto.Sku);
+
+        if (skuExists)
+            throw new ArgumentException($"Ya existe un producto con el codigo SKU: {dto.Sku}");
+
+        var newProduct = new Product
+        {
+            CompanyId = dto.CompanyId,
+            Sku = dto.Sku.Trim().ToUpper(),
+            Name = dto.Name.Trim(),
+            Description = dto.Description?.Trim(),
+            Price = dto.Price,
+            SalePrice = dto.SalePrice,
+            MinStockAlert = dto.MinStockAlert,
+            CategoryId = dto.CategoryId,
+            UnitId = dto.UnitId,
+            IsActive = dto.IsActive,
+            IsSoldOut = true
+        };
+        
+        var savedProduct = await _repository.AddAsync(newProduct);
+        
+        return new ProductLookUpDto(savedProduct.Id, savedProduct.Sku, savedProduct.Name);
+    }
+
+    public async Task<ProductLookUpDto> EditProductAsync(int productId, ProductUpdateDto dto)
+    {
+        var product = await _repository.GetByIdAsync(dto.CompanyId, productId);
+        if (product == null)
+            throw new KeyNotFoundException("El producto no existe");
+        
+        bool skuExists = await _repository.ExistsBySkuAsync(dto.CompanyId, dto.Sku, productId);
+        if (skuExists)
+            throw new InvalidOperationException($"El codigo SKU {dto.Sku} ya esta en uso por otro producto");
+        
+        product.Sku = dto.Sku.Trim().ToUpper();
+        product.Name = dto.Name.Trim();
+        product.Description = dto.Description?.Trim();
+        product.Price = dto.Price;
+        product.SalePrice = dto.SalePrice;
+        product.MinStockAlert = dto.MinStockAlert;
+        product.CategoryId = dto.CategoryId;
+        product.IsActive = dto.IsActive;
+        product.UnitId = dto.UnitId;
+        
+        await _repository.UpdateAsync(product);
+
+        return new ProductLookUpDto(productId, product.Sku, product.Name);
+    }
+
+    public async Task DesactiveProductAsync(int companyId, int productId)
+    {
+        var product = await _repository.GetByIdAsync(companyId, productId);
+        if (product == null)
+            throw new KeyNotFoundException("El producto no existe");
+
+        product.IsActive = false;
+        
+        await _repository.UpdateAsync(product);
+    }
+
+    public async Task ActivateProductAsync(int companyId, int productId)
+    {
+        var product = await _repository.GetByIdAsync(companyId, productId);
+        if (product == null)
+            throw new KeyNotFoundException("El producto no existe");
+
+        product.IsActive = true;
+        
+        await _repository.UpdateAsync(product);
     }
 }
