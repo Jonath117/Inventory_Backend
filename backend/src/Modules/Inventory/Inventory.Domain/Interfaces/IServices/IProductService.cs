5061ae1fecd32daa6ed2dfcda22d871a4671063f
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-03-30 02:38:28 -0400

    declaracion de metodos en la interfaz para poder realizar el crud y aplicar logica de negocio en Product

--- backend/src/Modules/Inventory/Inventory.Domain/Interfaces/IServices/IProductService.cs
+++ backend/src/Modules/Inventory/Inventory.Domain/Interfaces/IServices/IProductService.cs
@@ -5,4 +5,8 @@ namespace Inventory.Domain.Interfaces.IServices;
 public interface IProductService
 {
     Task<IEnumerable<ProductDto>> GetProductsAsync(int companyId);
+    Task<ProductLookUpDto> CreateProductAsync(ProductCreateDto dto);
+    Task<ProductLookUpDto> EditProductAsync(int productId, ProductUpdateDto dto);
+    Task DesactiveProductAsync(int companyId, int productId);
+    Task ActivateProductAsync(int companyId, int productId);
 }
