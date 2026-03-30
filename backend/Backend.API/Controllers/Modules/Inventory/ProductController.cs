c98cab90410dc02de57f1c372bd9ab6e09e270ef
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-03-30 02:31:38 -0400

    implementacion de los endpoints para el crud de Producto

--- backend/Backend.API/Controllers/Modules/Inventory/ProductController.cs
+++ backend/Backend.API/Controllers/Modules/Inventory/ProductController.cs
@@ -1,3 +1,4 @@
+using Inventory.Domain.DTOs;
 using Inventory.Domain.Interfaces.IServices;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +34,107 @@ public class ProductController: ControllerBase
             return StatusCode(500, new { error = "Ocurrió un error interno." });
         }
     }
-    
 
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromHeader(Name = "x-company-id")] int companyId,
+        [FromBody] ProductCreateDto dto)
+    {
+        try
+        {
+            if (companyId <= 0)
+            {
+                return BadRequest(new { error = "Id de compañia Invalido" });
+            }
+
+            var finalDto = dto with { CompanyId = companyId };
+            
+            var createdProduct = await _service.CreateProductAsync(finalDto);
+            
+            return Created("", createdProduct);
+            
+        } 
+        catch (InvalidOperationException ex) 
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al crear producto: {ex}");
+            return StatusCode(500, new { error = "Ocurrio un error interno en el servidor." });
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditProduct(int id, [FromHeader(Name = "x-company-id")] int companyId,
+        [FromBody] ProductUpdateDto dto)
+    {
+        try
+        {
+            if (companyId <= 0) return BadRequest(new { error = "Company Id invalido" });
+
+            var finalDto = dto with { CompanyId = companyId };
+            var updatedProduct = await _service.EditProductAsync(id, finalDto);
+
+            return Ok(updatedProduct);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message }); 
+        }
+        catch (InvalidOperationException ex) 
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = "Ocurrió un error interno." });
+        }
+    }
+
+    [HttpPatch("{id}/deactivate")]
+    public async Task<IActionResult> DeactivateProduct(int id, [FromHeader(Name = "x-company-id")] int companyId)
+    {
+        try
+        {
+            if (companyId <= 0) return BadRequest(new { error = "Company Id invalido" });
+            
+            await _service.DesactiveProductAsync(companyId, id);
+
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = "Ocurrio un error interno." });
+        }
+    }
+    
+    [HttpPatch("{id}/activate")]
+    public async Task<IActionResult> ActivateProduct(int id, [FromHeader(Name = "x-company-id")] int companyId)
+    {
+        try
+        {
+            if (companyId <= 0) return BadRequest(new { error = "Company Id invalido" });
+            
+            await _service.ActivateProductAsync(companyId, id);
+            
+            return NoContent();
+            
+        } 
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = "Ocurrio un error interno." });
+        }
+    }
 }
