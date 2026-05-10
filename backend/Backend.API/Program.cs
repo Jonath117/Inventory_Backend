e6a63c1170ae74a76faffb5a607ea8e6201d1801
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-10 17:12:24 -0400

    se modifico el program.cs para que solo conecte a todos los modulos pero cada Modulo se autoconfigura asi mismos asi se logra separar responsabilidades y desacoplar modulos

--- backend/Backend.API/Program.cs
+++ backend/Backend.API/Program.cs
@@ -1,80 +1,20 @@
 using Backend.API.Filters;
-using Microsoft.EntityFrameworkCore;
-using Inventory.Application.Services;
-using Inventory.Domain.Interfaces.IRepositories;
-using Inventory.Domain.Interfaces.IServices;
-using Inventory.Infrastructure.Data;
-using Inventory.Infrastructure.Repositories;
-using Sales.Domain.Interfaces;
-using Sales.Infrastructure.Persistence;
-using Sales.Infrastructure.Persistence.Repositories;
-using Shared.Application.Interfaces;
+using Inventory.Application;
+using Inventory.Infrastructure;
+using Sales.Application;
+using Sales.Infrastructure;
+using Shared.Application;
 using Shared.Infrastructure;
-using Shared.Infrastructure.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddInventoryInfrastructure(builder.Configuration);
+builder.Services.AddInventoryApplication();
+builder.Services.AddSalesInfrastructure(builder.Configuration);
+builder.Services.AddSalesApplication();
+builder.Services.AddCoreInfrastructure(builder.Configuration);
+builder.Services.AddCoreApplication();
 
-builder.Services.AddDbContext<InventoryDbContext>(options =>
-    options
-        .UseNpgsql(connectionString)
-        .UseSnakeCaseNamingConvention()
-    );
-
-Action<DbContextOptionsBuilder> npgsqlOptions = options => 
-    options.UseNpgsql(connectionString)
-        .UseSnakeCaseNamingConvention();
-
-
-builder.Services.AddDbContext<InventoryDbContext>(npgsqlOptions);
-builder.Services.AddDbContext<CoreDbContext>(npgsqlOptions);
-builder.Services.AddDbContext<SalesDbContext>(npgsqlOptions);
-
-builder.Services.AddScoped<IAdjustmentRepository, AdjustmentRepository>();
-builder.Services.AddScoped<IAdjustmentService, AdjustmentService>();
-
-builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
-builder.Services.AddScoped<ICompanyService, CompanyService>();
-
-builder.Services.AddScoped<IGetProductKardexRepository, GetProductKardexRepository>();
-builder.Services.AddScoped<IGetProductKardexService, GetProductKardexService>();
-
-builder.Services.AddScoped<IGetStockRepository, GetStockRepository>();
-builder.Services.AddScoped<IGetStockService, GetStockService>();
-
-builder.Services.AddScoped<ILookUpRepository, LookUpRepository>();
-builder.Services.AddScoped<ILookUpService, LookUpService>();
-
-builder.Services.AddScoped<IMovementRepository, MovementRepository>();
-builder.Services.AddScoped<IMovementService, MovementService>();
-
-builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<IUnitRepository, UnitRepository>();
-builder.Services.AddScoped<IUnitService, UnitService>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<ISalesRepository, SalesRepository>();
-builder.Services.AddMediatR(cfg => {
-    cfg.RegisterServicesFromAssembly(typeof(Sales.Application.Features.Tickets.CreateTicket.CreateTicketCommandHandler).Assembly);
-});
-
-builder.Services.AddScoped<ICurrentCompanyProvider, CurrentCompanyProvider>();
-
-builder.Services.AddControllers(options => 
-{
-    options.Filters.Add<CompanyTenantFilter>();
-});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -91,6 +31,11 @@ builder.Services.AddCors(options =>
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
+});
+
+builder.Services.AddControllers(options => 
+{
+    options.Filters.Add<CompanyTenantFilter>();
 });
 
 var app = builder.Build();
