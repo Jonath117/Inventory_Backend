61983ff59cc542ad3ac36282213b8f5a4e3fefc2
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-10 16:54:11 -0400

    implementacion de Extension Method sobre DependencyInjection donde van las Interfaces y sus Implementaciones relacionadas con el patron Repository

---
+++ backend/src/Modules/Inventory/Inventory.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+using Inventory.Domain.Interfaces.IRepositories;
+using Inventory.Infrastructure.Data;
+using Inventory.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Inventory.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInventoryInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<InventoryDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention();
+            
+        });
+        
+        services.AddScoped<IAdjustmentRepository, AdjustmentRepository>();
+        services.AddScoped<ICompanyRepository, CompanyRepository>();
+        services.AddScoped<IGetProductKardexRepository, GetProductKardexRepository>();
+        services.AddScoped<IGetStockRepository, GetStockRepository>();
+        services.AddScoped<ILookUpRepository, LookUpRepository>();
+        services.AddScoped<IMovementRepository, MovementRepository>();
+        services.AddScoped<IInventoryRepository, InventoryRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IUnitRepository, UnitRepository>();
+        
+        return services;
+    }
+    
+    
+}
