a093dc19afbda47773c68c8e658d758f651fd6ed
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-11 01:40:50 -0400

    archivo de configuracion para el mapeo

--- backend/src/Modules/Purchase.Infrastructure/Persistence/Configurations/PurchaseItemConfiguration.cs
+++ backend/src/Modules/Purchase.Infrastructure/Persistence/Configurations/PurchaseItemConfiguration.cs
@@ -1,6 +1,23 @@
-namespace Purchase.Infrastructure.Configurations;
+namespace Purchase.Infrastructure.Persistence.Configurations;
 
-public class PurchaseItemConfiguration
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
+
+public class PurchaseItemConfiguration : IEntityTypeConfiguration<PurchaseItem>
 {
-    
+    public void Configure(EntityTypeBuilder<PurchaseItem> builder)
+    {
+        builder.ToTable("purchase_items");
+
+        builder.HasKey(pi => pi.Id);
+
+        builder.Property(pi => pi.Quantity)
+            .IsRequired();
+        
+        builder.HasOne(pi => pi.Purchase)
+            .WithMany(p => p.Items)
+            .HasForeignKey(pi => pi.PurchaseId)
+            .OnDelete(DeleteBehavior.Cascade); 
+    }
 }
