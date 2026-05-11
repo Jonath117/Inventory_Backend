ed7c06a2bc34b90257bd321c5e4f257e667daba4
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-11 01:16:47 -0400

    se implemento el detalle de compra y se agrego la propiedad de navegacion aunque es algo redundante

---
+++ backend/src/Modules/Purchase.Domain/Entities/PurchaseItem.cs
@@ -0,0 +1,18 @@
+namespace Purchase.Domain.Entities;
+
+public class PurchaseItem
+{
+    public int Id { get; private set; }
+    public int PurchaseId { get; private set; }
+    public int ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public Purchase Purchase { get; private set; }
+    
+    private PurchaseItem() { }
+
+    internal PurchaseItem(int productId, int quantity)
+    {
+        ProductId = productId;
+        Quantity = quantity;
+    }
+}
