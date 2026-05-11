be05b0b74d08e160063ee24f8ddbd6a5590d6495
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-11 01:40:15 -0400

    se configuro la coneccion con la bdd

--- backend/src/Modules/Purchase.Infrastructure/Persistence/PurchaseDbContext.cs
+++ backend/src/Modules/Purchase.Infrastructure/Persistence/PurchaseDbContext.cs
@@ -1,6 +1,23 @@
-namespace Purchase.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Purchase.Domain.Entities;
 
-public class PurchaseDbContext
+namespace Purchase.Infrastructure.Persistence;
+
+public class PurchaseDbContext : DbContext
 {
-    
+    public PurchaseDbContext(DbContextOptions<PurchaseDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Purchase.Domain.Entities.Purchase> Purchases { get; set; }
+    public DbSet<PurchaseItem> PurchaseItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.HasDefaultSchema("purchases");
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PurchaseDbContext).Assembly);
+    }
 }
