a8a521f2abeece5732b8901d33f49369f10b2623
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-13 10:51:29 -0400

    implementacion de un metodo para guardar en la bdd

---
+++ backend/src/Modules/Purchase/Purchases.Application/Interfaces/Repositories/IPurchasesRepository.cs
@@ -0,0 +1,9 @@
+using Purchases.Domain.Entities;
+
+namespace Purchases.Application.Interfaces.Repositories;
+
+public interface IPurchasesRepository
+{
+    Task<Purchase> CreateAsync(int companyId, Purchase purchase);
+    
+}  
