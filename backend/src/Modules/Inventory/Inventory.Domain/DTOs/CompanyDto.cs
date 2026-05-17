ab7457b97eb2baa19e4357792b727d3ce3b84a1c
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-16 21:41:16 -0400

    se agrego el companyCen para retornarlo en el get de las companies

--- backend/src/Modules/Inventory/Inventory.Domain/DTOs/CompanyDto.cs
+++ backend/src/Modules/Inventory/Inventory.Domain/DTOs/CompanyDto.cs
@@ -3,5 +3,6 @@ namespace Inventory.Domain.DTOs;
 public class CompanyDto
 {
     public int Id { get; set; }
+    public string CompanyCen { get; set; }
     public string Name { get; set; } = string.Empty;
 }
