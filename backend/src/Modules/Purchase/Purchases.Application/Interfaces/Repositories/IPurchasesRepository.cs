540516c2827a0314823cb2f707bf0c51a1370cbc
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-18 03:50:55 -0400

    declaracion del metodo usado para paginar y devolver una lista de ordenes de compra

--- backend/src/Modules/Purchase/Purchases.Application/Interfaces/Repositories/IPurchasesRepository.cs
+++ backend/src/Modules/Purchase/Purchases.Application/Interfaces/Repositories/IPurchasesRepository.cs
@@ -5,4 +5,12 @@ namespace Purchases.Application.Interfaces.Repositories;
 public interface IPurchasesRepository
 {
     Task<Purchase> CreateAsync(int companyId, Purchase purchase);
+    
+    Task<(int TotalCount, IEnumerable<Purchase> Items)> GetPagedOrdersAsync(
+        int companyId, 
+        int? status, 
+        int page, 
+        int pageSize, 
+        bool sortDescending, 
+        CancellationToken cancellationToken);
 }  
